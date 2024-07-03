247b35b438345d47137dd59e46f467f28d44df6a
Sergoman4 <28062002sergey@gmail.com>
2024-07-03 15:47:33 +1000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,18 +1,22 @@
-def ancient_cipher(n):
+def generate_password(n):
+    if not 3 <= n <= 20:
+        return "Число должно быть от 3 до 20."
 
-  if 1 <= n <= 20:
-    result = ""
-    for i in range(1, 10):
-      for j in range(i, 10):
-        if (i + j) != 0 and n % (i + j) == 0:
-          result += str(i) + str(j)
-    return result
+    password = ""
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if (i + j) != 0 and n % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
 
 while True:
-  n = int(input("Введите число из первой каменной вставки (от 1 до 20, или другое число для выхода): "))
-
-  if 1 <= n <= 20:
-    password = ancient_cipher(n)
-    print(f"Пароль для числа {n}: {password}")
-  else:
-    break
+    try:
+        n = int(input("Введите число из первой каменной вставки (от 3 до 20): "))
+        if 3 <= n <= 20:
+            password = generate_password(n)
+            print(f"Пароль для числа {n}: {password}")
+            break
+        else:
+            print("Некорректное число. Введите число от 3 до 20.")
+    except ValueError:
+        print("Некорректный ввод. Введите число.")
